4450336735a81a354f2cfb4e6da4a2683f069d5c
tharindumayuranga <tharindumayuranga@users.noreply.github.com>
2023-01-23 14:50:35 +0530

    [INIT] - Fixed Assets Management Module

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Fixed Assets Management',
+    'version': '15.0.0.1',
+    'sequence': 200,
+    'category': 'Uncategorized ',
+    'website': 'https://optimized.lk/',
+    'author': 'OPTIMIZED TECHNOLOGIES',
+    'summary': 'Manage all  fixed assets of the company',
+    'depends': [
+        'base', 
+    ],
+    'data': [
+    
+    ],
+    
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import res_partner
+from . import server_types
+from . import server_details
+from . import purchase
